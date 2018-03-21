8b12f83079172743c56386bf7761a9a86f8fd818
ledkis-mamanPc <emile.quenee@gmail.com>
2018-03-21 18:37:52 +0100

    add fetch pape from json

--- wip.js
+++ wip.js
@@ -13,43 +13,62 @@ logger.info(`NODE_ENV: ${NODE_ENV}`)
 function fetchPapesList(opts){
 	const url = "http://fr.wikipedia.org/w/api.php?action=parse&page=Liste_des_papes&prop=categories%7Cexternallinks%7Clinks%7Ctext&lang=fr&redirects=true&format=json";
 	const page = "Liste_des_papes";
-	//const page = "List_of_tz_database_time_zones";
 
 	logger.info(`fetchPapesList: ${page}, opts: ${JSON.stringify(opts)}`);
 
+    //no need to save the file if we fetch from it
+    if(opts.from === 'json') opts.save = false;
+
 	return new Promise((resolve, reject) => {
-        if(opts.online){
-         wikipedia.page.data(page, { content: true , lang:'fr'}, (response, page, url) => {
-                // TODO: Promise && Remove try catch ?
-                try{
-                    if(response.error) return reject(response.error);
+        switch (opts.from) {
+            case 'wiki':
+                wikipedia.page.data(page, { content: true , lang:'fr'}, (response, page, url) => {
+                    // TODO: Promise && Remove try catch ?
+                    try{
+                        if(response.error) return reject(response.error);
 
-                    resolve(scrapPapesPage(response.parse, page, url));
-                } catch(err){
-                    reject(err);
-                }
-            });
-         } else {
-            fs.readFile(`./data/raw/${page}.html`, 'utf8', (err, data) => {
+                        resolve(scrapPapesPage(response.parse, page, url));
+                    } catch(err){
+                        reject(err);
+                    }
+                });
+                break;
+    
+            case 'raw':
+                fs.readFile(`./data/raw/${page}.html`, 'utf8', (err, data) => {
 
-                try{
-                    if (err) return reject(err);
+                    try{
+                        if (err) return reject(err);
 
-                    const response = {text: {}};
-                    response.text["*"] = data;
+                        const response = {text: {}};
+                        response.text["*"] = data;
 
-                    resolve(scrapPapesPage(response, page, url));
-                } catch(err){
-                    reject(err);
-                }
-            });
+                        resolve(scrapPapesPage(response, page, url));
+                    } catch(err){
+                        reject(err);
+                    }
+                });
+                break;
+
+            case 'json':
+            default:
+                fs.readFile(`./data/json/${page}.json`, 'utf8', (err, data) => {
+
+                        try{
+                            if (err) return reject(err);
+
+                            resolve(JSON.parse(data));
+                        } catch(err){
+                            reject(err);
+                        }
+                    });
         }
     }).then((res, err) => {
         if(err) throw err;
 
         if(opts.save){
             return new Promise(function(resolve, reject) {
-                fs.writeFile(`data/json/${res.title}.json`, JSON.stringify(res, null, 4), function(err) {
+                fs.writeFile(`./data/json/${res.title}.json`, JSON.stringify(res, null, 4), function(err) {
                     if (err) return reject(err);
                     logger.info(`data/json/${res.title}.json saved`);
                     resolve(res);
@@ -158,7 +177,7 @@ function scrapPapesPage(response, page, url){
 
 
 
-fetchPapesList({online: !(NODE_ENV === 'debug'), save:true}).then((res, err) => {
+fetchPapesList({from:'json', save:true}).then((res, err) => {
     if(err) throw err;
     logger.info(`papes nbr: ${res.list.length}`)
 }).catch(error => { 
