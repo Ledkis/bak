991267290ba534399b970b58f8f1c794308a3208
ledkis-mamanPc <emile.quenee@gmail.com>
2018-03-21 13:54:38 +0100

    wip fetchPapesList 1st implementation

--- wip.js
+++ wip.js
@@ -14,47 +14,111 @@ logger.info(`NODE_ENV: ${NODE_ENV}`)
 function fetchPapesList(opts){
 	const url = "http://fr.wikipedia.org/w/api.php?action=parse&page=Liste_des_papes&prop=categories%7Cexternallinks%7Clinks%7Ctext&lang=fr&redirects=true&format=json";
 	const page = "Liste_des_papes";
+	//const page = "List_of_tz_database_time_zones";
+
+	logger.info(`fetchPapesList: ${page}`);
 
 	return new Promise((resolve, reject) => {
 		let res;
 
-		if(opts.online){
-			wikipedia.page.data(page, { content: true , lang:'fr'}, (response, page, url) => {
-				res = scrapPapesPage(response, page, url);			
-			});
-		} else {
-			const pageData = fs.readFileSync(`./data/raw/${page}.html`, 'utf8');
-			const response = {text: {}};
+        if(opts.online){
+           wikipedia.page.data(page, { content: true , lang:'en'}, (response, page, url) => {
+                // todo: Promise ?
+                res = scrapPapesPage(response, page, url);			
+            });
+       } else {
+           const pageData = fs.readFileSync(`./data/raw/${page}.html`, 'utf8');
+           const response = {text: {}};
 
-			response.text["*"] = pageData;
+           response.text["*"] = pageData;
 
-			res = scrapPapesPage(response, page, url);
-		}
+           res = scrapPapesPage(response, page, url);
+       }
 
-		resolve(res);
-	}).catch(error => { logger.err(error); });
-;
+       logger.info(`papes nbr: ${res.list.length}`)
+
+       resolve(res);
+   }).catch(error => { logger.err(error); });
+	;
 }
-
 
 function scrapPapesPage(response, page, url){
-	const data = {};
-	data.title = page;
-	data.url = url;
 
-	data.links = [];
+    // Prepare result
+    const data = {};
+    data.title = page;
+    data.url = url;
 
-	const $ = cheerio.load(response.text["*"]);
+    data.list = [];
 
-	let h2Section, h3Section, h4Section;
-	let context = {title : ''};
-	$('.mw-parser-output')[0].children.forEach((el) => {
+    try{
 
-		
-	});
+        const $ = cheerio.load(response.text["*"]);
 
-	return data;
+        // get papes table
+        var tables = $('.wikitable');
+
+        // Validate result before parsing
+        if (tables.length === 0) {
+            throw new Error('Unable to find any data!');
+            return;
+        }
+
+        //Loop on the tables
+        tableloop: for(let idTable = 0; idTable < tables.length; idTable++){
+            const table = tables[idTable];
+            const rows = $(table).find('tr');
+
+            let fiedsTagList = [];
+            //Loop on the rows
+            rowloop: for(let idRow = 0; idRow < rows.length; idRow++){
+                const row = rows[idRow];
+
+                if(idRow === 0){
+
+                    const fieldsTag = $(row).find('th');
+
+                    //Loop on field tag
+                    for(let idField = 0; idField < fieldsTag.length; idField++){
+                        let node = fieldsTag[idField];
+                        if(node.children.length === 1){
+                            fiedsTagList[idField] = node.children[0].data;
+                        } else if(node.children.length === 2){
+                                fiedsTagList[idField] = node.children[0].attribs.title;
+                        } else {
+                            // Not a pape table
+                        }
+                    }
+
+                    continue rowloop;
+                }
+
+                const fields = $(row).find('td');
+
+                if(fields.length !== fiedsTagList.length) continue tableloop;
+
+                const pape = {};
+
+                //Loop on field
+                for(let idField = 0; idField < fiedsTagList.length; idField++){
+                    pape[fiedsTagList[idField]] = $(fields[idField].children[0]).text();
+                }
+
+                //console.log(pape);
+
+                data.list.push(pape);
+            }
+        }
+
+        return data;
+
+    } catch(err){
+        err.message = `Error while parsing papes: ${err.message}`;
+        logger.err(err);
+        return data;
+    }
 }
+
 
 
 fetchPapesList({online: !(NODE_ENV === 'debug')});
