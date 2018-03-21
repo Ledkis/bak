4d7c8f32b9cb633475693d1f161b394192c19421
ledkis-mamanPc <emile.quenee@gmail.com>
2018-03-21 18:57:17 +0100

    fix some logging

--- bak.js
+++ bak.js
@@ -2,7 +2,10 @@ const config = require('config');
 const logger = require ('./lib/my-winston')('bak');
 const wikimanager = require('./lib/wikimanager');
 
-logger.info(`*** BAK: START***, NODE_ENV: ${config.get('NODE_ENV')}`)
+const NODE_ENV = config.get('NODE_ENV');
+
+logger.info(`____________BAK: START____________`);
+logger.info(`NODE_ENV: ${NODE_ENV}`)
 wikimanager.main();
 
 // node --inspect-brk bak.js
