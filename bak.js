aefca8e95e5bc9e5cde92b0e5548696c8b42b654
ledkis-mamanPc <emile.quenee@gmail.com>
2018-03-24 10:29:30 +0100

    add wikimanager to wikiapi

--- bak.js
+++ bak.js
@@ -1,11 +1,20 @@
 const config = require('config');
 const logger = require ('./lib/my-winston')(__filename);
-const wikimanager = require('./lib/wikimanager');
+const wikiapi = require('./lib/wikiapi');
 
 const NODE_ENV = config.get('NODE_ENV');
 
 logger.info(`____________BAK: START____________`);
 logger.info(`NODE_ENV: ${NODE_ENV}`)
-wikimanager.main();
+
+// //Sync is ok here
+// const dataInfo = datamanager.getDataInfoSync();
+
+// const opts = {type:type, from:'wiki', save:true, page:dataInfo[type].page};
+
+// // TODO moche : opts & dataInfo
+// wikiapi.fetchWikiData(opts, dataInfo).catch(error => logger.err(error));
+
+wikiapi.updateData(false);	
 
 // node --inspect-brk bak.js
