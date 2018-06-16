a1f0d7c97f4c724b6a7eb5af4cd036c5fde28996
ledkis-W520 <emile.quenee@gmail.com>
2018-06-16 12:09:51 +0200

    wikiapi updateLocations 1st implementation

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
 ## Snippets
 
-var wikiapi = require('./app_api/wikiapi')
-wikiapi.updateParsing()
+var moment = require('./lib/my-moment')
+var logger = require('../lib/my-winston')('REPL')
 
-var wikiapi = require('./app_api/wikiapi')
-wikiapi.updateData()
+
+require('./app_api/wikiapi').updateParsing()
+require('./app_api/wikiapi').updateData()
+require('./app_api/wikiapi').updateLocations()
+require('./lib/apiutils').getLocation()
 
 var wikiapi = require('./app_api/wikiapi'); var data;
 wikiapi.fetchWikiData({dataId: 'monarques_fr', from: 'wiki'}).then((wikiData) => {
   data = wikiData
 })
 
-var moment = require('./lib/my-moment')
+
+## link
+
+https://developers.google.com/maps/pricing-and-plans/
+
+## api
+
+mail: emile.quenee@gmail.com
+project name: bbak
+API_KEY: AIzaSyAq-SoSY8Dw59k9wK-n0bUc9_YQbcbAcUQ
--- app_api/wikiapi.js
+++ app_api/wikiapi.js
@@ -1,8 +1,10 @@
+const fs = require('fs')
 const wikipedia = require('../lib/my-node-wikipedia')
 const logger = require('../lib/my-winston')(__filename)
 const datamanager = require('./datamanager')
 const wikiscrapper = require('../lib/wikiscrapper')
 const wikiparser = require('../lib/wikiparser')
+const apiutils = require('../lib/apiutils')
 
 const wikiapi = {}
 
@@ -63,8 +65,7 @@ wikiapi.updateData = function () {
   const dataInfo = datamanager.getDataInfo()
 
   let dataToUpdate = []
-
-  const promises = []
+  let promises = []
 
   Object.keys(dataInfo).forEach((dataId) => {
     const wikiDataInfo = dataInfo[dataId]
@@ -74,7 +75,7 @@ wikiapi.updateData = function () {
     dataToUpdate.push(dataId)
     const p = this.fetchWikiData(opts).then(() => {
       logger.info(`updateData: ${dataId} updated`)
-    }).catch(error => logger.err(error))
+    }).catch(err => logger.err(err))
 
     promises.push(p)
   })
@@ -93,7 +94,8 @@ wikiapi.updateParsing = function () {
   const dataInfo = datamanager.getDataInfo()
 
   let dataToUpdate = []
-  const promises = []
+  let promises = []
+
   Object.keys(dataInfo).forEach((dataId) => {
     const wikiDataInfo = dataInfo[dataId]
 
@@ -105,17 +107,107 @@ wikiapi.updateParsing = function () {
       wikiparser.parseWikiData(wikiData, wikiDataInfo)
       datamanager.saveWikiDataJSON(opts.dataId, wikiData)
       logger.info(`updateParsing: ${dataId} updated`)
-    }).catch(error => logger.err(error))
+    }).catch(err => logger.err(err))
 
     promises.push(p)
   })
+
+  logger.info(`updateParsing: to update : ${JSON.stringify(dataToUpdate)}`)
 
   Promise.all(promises)
     .then(() => {
       logger.info(`updateParsing: end`)
     })
+}
 
-  logger.info(`updateParsing: to update : ${JSON.stringify(dataToUpdate)}`)
+wikiapi.getLocation = function (wikiObj){
+
+  let locations = {}
+  let promises = []
+  let searchedLocations = []
+
+  let locTypesToGet = ['birthPlace', 'deathPlace']
+
+  locTypesToGet.forEach(locType => {
+    if(wikiObj[locType]) {
+      let p = apiutils.getLocation(wikiObj.birthPlace).then(res => {
+        wikiObj[`${locType}Lat`] = res.geometry.location.lat
+        wikiObj[`${locType}Lng`] = res.geometry.location.lng
+        locations[res.place_id] = res
+      }).catch(err => logger.err(err, `getLocation: ${wikiObj.name} ${locType} ${wikiObj.birthPlace}`))
+      promises.push(p)
+      searchedLocations.push(wikiObj[locType])
+    }
+  })
+
+  return Promise.all(promises)
+    .then(() => {
+      if(wikiObj.name && searchedLocations.length > 0)
+        logger.info(`getLocation: ${wikiObj.name}: [${searchedLocations.join(' ')}] completed`)
+      return locations
+    })
+}
+
+wikiapi.getAllLocations = function (wikiData) {
+  logger.info(`getAllLocations`)
+
+  let promises = []
+  let locations = {}
+
+  wikiData.list.forEach(wikiObj => {
+    let p = wikiapi.getLocation(wikiObj).then(locs => {
+      locations = Object.assign(locations, locs)
+      return locations
+    })
+    promises.push(p)
+  })
+
+  return Promise.all(promises)
+    .then(() => {
+      logger.info(`getAllLocations: ${wikiData.dataId} completed`)
+      datamanager.saveWikiDataJSON(wikiData.dataId, wikiData)
+      return locations
+    })
+}
+
+wikiapi.updateLocations = function () {
+  logger.info(`updateLocations`)
+
+  const dataInfo = datamanager.getDataInfo()
+
+  let dataToUpdate = []
+  let promises = []
+
+  let locations = {}
+
+  Object.keys(dataInfo).forEach((dataId) => {
+    const wikiDataInfo = dataInfo[dataId]
+
+    const opts = {dataId: dataId, from: 'json', page: wikiDataInfo.page}
+
+    // if(dataId === 'monarques_en'){
+      dataToUpdate.push(dataId)
+
+      const p = this.fetchWikiData(opts, dataInfo)
+        .then(wikiData => {
+          //wikiData.list = wikiData.list.filter(el => el.deathPlace).slice(0, 4)
+          return wikiapi.getAllLocations(wikiData)
+        }).then(locs => {
+          locations = Object.assign(locations, locs)
+          return locations
+        })
+
+      promises.push(p)
+    // }
+  })
+
+  logger.info(`updateLocations: to update : ${JSON.stringify(dataToUpdate)}`)
+
+  return Promise.all(promises)
+    .then(() => {
+      fs.writeFileSync('places.json', JSON.stringify(locations, null, 4))
+      logger.info(`updateLocations: completed with ${Object.keys(locations).length} new loc saved`)
+    })
 }
 
 module.exports = wikiapi
