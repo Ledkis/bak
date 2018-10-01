46902099d11a826c35b6f9b15717f9a296778b7e
ledkis-W520 <emile.quenee@gmail.com>
2018-10-01 13:41:06 +0200

    remove useless files

--- wip.js
+++
@@ -1,9 +0,0 @@
-const config = require('config')
-const logger = require('./lib/my-winston')(__filename)
-
-const NODE_ENV = config.get('NODE_ENV')
-
-logger.info(`____________WIP: START____________`)
-logger.info(`NODE_ENV: ${NODE_ENV}`)
-
-// node --inspect-brk wip.js
